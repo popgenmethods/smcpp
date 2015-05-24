ec1ac7652e16868f87087f7eec9e75c336c9240c
Jonathan Terhorst <terhorst@gmail.com>
2015-05-24 13:20:44 -0700

    fixed up some test code

--- test/fixtures.py
+++ test/fixtures.py
@@ -17,3 +17,10 @@ def constant_demo_1000():
     b = np.array([1e-6])
     s = np.array([0.0])
     return (a, b, s)
+
+@pytest.fixture
+def demo():
+    a = np.array([1., 2., 3., 4.])
+    b = np.array([.01, -.00001, 0.02, 0.0])
+    s = np.array([0.0, 0.2, 0.4, 0.3])
+    return (a, b, s)
--- test/test_inference.py
+++ test/test_inference.py
@@ -6,14 +6,12 @@ import logging
 logging.getLogger().setLevel("INFO")
 
 import inference
+from fixtures import *
 
 N = 10
 L = 1000
-hidden_states = np.array([0.0, 1.0, 2.0, np.inf])
-
-@pytest.fixture
-def demo():
-    return ([1.0, 2.0], [.0001, -.01], [0.0, 0.5])
+hidden_states = np.array([0.0, 0.5, 1.0, 2.0, 3.0, 4.0, np.inf])
+NTHREADS = 4
 
 @pytest.fixture
 def fake_obs():
@@ -24,31 +22,39 @@ def fake_obs():
         ary.append([1, d, np.random.randint(not d, N + 1 - (d == 2))])
     return np.array(ary)
 
-def test_inference_parallel(demo, fake_obs):
+def test_derivatives(demo, fake_obs):
     a, b, s = demo
+    S = 1000
+    M = 100
+    N0 = 1000.
+    rho = theta = 1e-8
+    obs_list = [fake_obs]
+    def f(a, b, s, jacobian):
+        return inference.loglik(
+                a, b, s,
+                N,
+                S, M,
+                obs_list, # List of the observations datasets we prepared above
+                hidden_states,
+                N0 * rho, # Same parameters as above
+                N0 * theta, 
+                reg_a=0., reg_b=0., reg_s=0.,
+                numthreads=NTHREADS, seed=1, viterbi=False, jacobian=jacobian)
+    logp, jac = f(a, b, s, True)
+    print(jac)
+    eps = 0.1
+    K = len(a)
+    I = np.eye(K)
+    for k in [0, 1, 2]:
+        for ell in range(K):
+            args = [a.copy(), b.copy(), s.copy()]
+            args[k][ell] += eps
+            logp2 = f(args[0], args[1], args[2], False)
+            print(k, ell, logp2, logp, jac[k, ell], logp2 - (logp + eps * jac[k, ell]))
+
+def test_inference_parallel(constant_demo_1, fake_obs):
+    a, b, s = constant_demo_1
     ll, jac = inference.loglik(a, b, s, 1, 1, N, [fake_obs,] * 4, 1e-8, 1e-8, hidden_states, numthreads=8, jacobian=True)
     print(ll)
     print(jac)
     # Well, that worked
-
-def test_inference(demo, fake_obs):
-    logp, jac = inference.logp(demo.sqrt_a, demo.b, demo.sqrt_s, 1000, 500, N, fake_obs, 1e-8, 1e-8, hidden_states)
-    print(logp)
-    print(jac)
-    # Well, that worked
-
-def test_derivatives(demo, fake_obs):
-    logp, jac = inference.logp(demo.sqrt_a, demo.b, demo.sqrt_s, 500, 100, N, fake_obs, 1e-8, 1e-8, hidden_states)
-    print(jac)
-    eps = 0.01
-    I = np.eye(demo.K)
-    for k in [0, 1, 2]:
-        for ell in range(demo.K):
-            if k == 2 and ell == 0:
-                continue
-            args = [demo.sqrt_a, demo.b, demo.sqrt_s, demo.hs]
-            delta = args[k][ell] * eps
-            args[k][ell] *= (1 + eps)
-            demo2 = Demography(*args)
-            logp2, _ = inference.logp(demo2, 100, 1000, N, fake_obs, 1e-8, 1e-8, hidden_states)
-            print(k, ell, logp2, logp, delta, logp2 - (logp + delta * jac[k, ell]))
