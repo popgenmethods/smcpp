3f6fe08126a8cbcb25db79a2864c908f59d31672
Jonathan Terhorst <terhorst@gmail.com>
2015-10-08 13:04:54 -0700

    fixed a bug in exponential intergrals

--- src/exponential_integrals.cpp
+++ src/exponential_integrals.cpp
@@ -1,52 +1,35 @@
 #include "exponential_integrals.h"
 
+#ifndef EINTDIFF_QUAD
 template <>
 mpfr::mpreal expintei(const mpfr::mpreal &x, const mpfr::mpreal &y)
 {
-    return mpf_ei(x, y, x.getPrecision());
+    return mpf_ei(x, true, x.getPrecision()) * exp(x + y);
 }
 
-/*
-template <>
-adouble expintei(const adouble&)
-{
-    throw std::domain_error("what");
-}
-
-template <>
-mpreal_wrapper<adouble> expintei(const mpreal_wrapper<adouble> &)
-{
-    throw std::domain_error("what");
-}
-*/
-
+#include "gsl/gsl_sf_expint.h"
 template <>
 double expintei(const double &x, const double &y)
 {
+    gsl_sf_result res;
+    gsl_sf_expint_Ei_scaled_e(x, &res);
+    return exp(x + y) * res.val;
     mpfr::mpreal xd(x), yd(y);
     xd.setPrecision(53);
     yd.setPrecision(53);
     return expintei(xd, yd).toDouble();
 }
 
-#ifndef EINTDIFF_QUAD
 template <>
-double eintdiff(const double &a, const double &b, const double &r)
+double eintdiff<double>::run(const double &a, const double &b, const double &r)
 {
-    mpfr::mpreal ma(a, 70), mb(b, 70), mr(r, 70);
-    return eintdiff(ma, mb, mr).toDouble();
-}
-
-template <typename T>
-Eigen::AutoDiffScalar<T> eintdiff(const Eigen::AutoDiffScalar<T> a, 
-        const Eigen::AutoDiffScalar<T> b, const Eigen::AutoDiffScalar<T> c)
-{
-    Eigen::AutoDiffScalar<T> ret;
-    ret.value() = eintdiff(a.value(), b.value(), c.value());
-    ret.derivatives() = exp(c.value()) * (exp(b.value()) / b.value() * b.derivatives() - exp(a.value()) / a.value() * a.derivatives());
-    ret.derivatives() += c.derivatives() * ret.value();
+    // mpfr::mpreal ma(a, 70), mb(b, 70), mr(r, 70);
+    // double ret = eintdiff<mpfr::mpreal>::run(ma, mb, mr).toDouble();
+    double ret = expintei(b, r) - expintei(a, r);
+    // std::cout << "eintdiff: " << ret << " " << exp(r) * (gsl_sf_expint_Ei(b) - gsl_sf_expint_Ei(a)) << std::endl;
     return ret;
 }
+
 #endif
 
 
@@ -55,20 +38,33 @@ Eigen::AutoDiffScalar<T> eintdiff(const Eigen::AutoDiffScalar<T> a,
 // The author of that code is Fredrik Johansson (https://github.com/fredrik-johansson)
 // Any mistakes are my fault.
 
-inline mpz_class to_fixed(mpfr::mpreal x, mp_prec_t wp)
+struct libmp_repr
 {
-    mp_exp_t e;
-    mpz_class ret;
-    mpfr::mpreal man = mpfr::frexp(x, &e);
-    mp_prec_t offset = wp + e;
-    man *= sgn(x);
-    if (offset >= 0) 
-        man <<= offset;
-    else 
-        man >>= -offset;
-    mpfr_get_z(ret.get_mpz_t(), man.mpfr_srcptr(), mpfr::mpreal::get_default_rnd());
-    return ret;
+    libmp_repr(const mpfr::mpreal &x) : 
+        man(), expo(mpfr_get_z_exp(man.get_mpz_t(), x.mpfr_srcptr())),
+        bc(mpz_sizeinbase(man.get_mpz_t(), 2))
+    {
+        sgn = mpz_sgn(man.get_mpz_t());
+        man *= sgn;
+        sgn = (sgn == -1) ? 1 : 0;
+    }
+    mpz_class man;
+    mp_prec_t expo;
+    int sgn;
+    mp_prec_t bc;
+};
+
+inline mpz_class to_fixed(const libmp_repr &x, const mp_prec_t prec)
+{
+    mp_prec_t offset = prec + x.expo;
+    if (x.sgn)
+        if (offset >= 0) return (-x.man) << offset;
+        else return (-x.man) >> (-offset);
+    else
+        if (offset >= 0) return x.man << offset;
+        else return x.man >> (-offset);
 }
+
 
 mpz_class ei_taylor(mpz_class x, mp_prec_t prec)
 {
@@ -101,63 +97,105 @@ mpz_class ei_asymptotic(mpz_class x, mp_prec_t prec)
 
 mpfr::mpreal mpf_ei(const mpfr::mpreal &x, const mp_prec_t prec)
 {
-    return mpf_ei(x, 0, prec);
+    return mpf_ei(x, false, prec);
 }
 
-mpfr::mpreal mpf_ei(const mpfr::mpreal &_x, const mpfr::mpreal &log_coef, const mp_prec_t prec)
+mpfr::mpreal mpf_ei(const mpfr::mpreal &_x, bool scaled, const mp_prec_t prec)
 {
-    mpfr::mpreal x = _x;
-    /*
-    if (x > 0)
-    {
-        x.setPrecision(prec);
-        return mpfr::eint(x);
-    }
-    */
-    mp_exp_t expo;
-    mpfr::mpreal man = mpfr::frexp(x, &expo);
-    mp_prec_t bc = x.getPrecision();
-    mp_prec_t xmag = bc + expo;
+    libmp_repr x(_x);
+    mpfr::mpreal v("0", prec);
+    mp_prec_t xmag = x.expo + x.bc;
     mp_prec_t wp = prec + 20;
     bool can_use_asymp = xmag > wp;
-    mpfr::mpreal xabsint, v;
-    mpz_class tmp;
-    mpfr::mpreal xprec(x);
-    xprec.setPrecision(wp);
     if (not can_use_asymp)
     {
-        if (expo >= 0)
-            xabsint = man << expo;
+        mpz_class xabsint;
+        if (x.expo >= 0)
+            xabsint = x.man << x.expo;
         else
-            xabsint = man >> (-expo);
+            xabsint = x.man >> (-x.expo);
         can_use_asymp = xabsint > int(wp * 0.693) + 10;
     }
+    mpfr::mpreal xp(_x);
+    xp.setPrecision(prec);
     if (can_use_asymp)
     {
-        v = (xmag > wp) ? "1" : mpfr::ldexp(mpfr::mpreal(ei_asymptotic(to_fixed(x, wp), wp).get_mpz_t(), wp), -wp);
-        v *= exp(xprec + log_coef) / xprec;
+        if (xmag > wp)
+            v = "1";
+        else
+        {
+            mpz_class eia = ei_asymptotic(to_fixed(x, wp), wp);
+            mpfr_set_z_2exp(v.mpfr_ptr(), eia.get_mpz_t(), -wp, MPFR_RNDN);
+        }
+        if (not scaled)
+            v *= exp(xp);
+        v /= xp;
     }
     else
     {
-        wp += 2 * (int)(abs(x).toLong());
+        wp += 2 * (int)(abs(_x).toLong());
         mpz_class u = to_fixed(x, wp);
-        mpz_class u2 = ei_taylor(u, wp) + to_fixed(mpfr::const_euler(wp), wp);
-        mpfr::mpreal t1 = mpfr::ldexp(mpfr::mpreal(u2.get_mpz_t(), wp), -wp);
-        mpfr::mpreal t2 = log(abs(xprec));
+        mpz_class eul = to_fixed(libmp_repr(mpfr::const_euler(wp)), wp);
+        mpz_class u2 = ei_taylor(u, wp);
+        u2 += eul;
+        mpfr::mpreal t1("0", wp); mpfr_set_z_2exp(t1.mpfr_ptr(), u2.get_mpz_t(), -wp, MPFR_RNDN);
+        xp.setPrecision(wp);
+        mpfr::mpreal t2 = log(abs(xp));
         v = t1 + t2;
-        v *= exp(log_coef);
+        if (scaled)
+            v *= exp(-xp);
     }
+    v.setPrecision(prec);
     return v;
 }
 
-/*
 #include "gsl/gsl_sf_expint.h"
-int main(int argc, char** argv)
+int eint_main(int argc, char** argv)
 {
-    for (int i=-10; i < 11; ++i)
-    {
-        double x = pow(2.5, i);
-        std::cout << mpf_ei(x, (mp_prec_t)53).toDouble() << " " << gsl_sf_expint_Ei(x) << std::endl;
-    }
+    /*
+    double ada = 0.9996804464437794, adb = -0.031297501072304855, Rrng = 0,
+           y = -0.035036331483659006, tmp = 0.011684646627566872;
+    double adab = ada / adb;
+    double c1 = 2 * adab * exp(adb * tmp);
+    double c2 = 2 * adab;
+    double c3 = 2 * (Rrng - adab) + y;
+    std::cout << c1 << " " << mpf_ei(c1, (mp_prec_t)53) << " " << gsl_sf_expint_Ei(c1) << std::endl;
+    std::cout << c1 << " " << mpf_ei(c2, (mp_prec_t)53) << " " << gsl_sf_expint_Ei(c2) << std::endl;
+    */
+    /*
+    Vector<double> d(3);
+    d(0) = 1;
+    d(1) = 0;
+    d(2) = 0;
+    adouble x(1.0, d);
+    d(0) = 0; d(1) = 1;
+    adouble y(2.0, d);
+    d(1) = 0; d(2) = 1;
+    adouble z(3.0, d);
+    adouble d1 = eintdiff<adouble>::run(x, y, z);
+    double dx = eintdiff<double>::run(x.value() + 1e-8, y.value(), z.value());
+    double dy = eintdiff<double>::run(x.value(), y.value() + 1e-8, z.value());
+    double dz = eintdiff<double>::run(x.value(), y.value(), z.value() + 1e-8);
+    std::cout << d1.derivatives() << std::endl;
+    std::cout << (dx - d1.value()) * 1e8 << std::endl;
+    std::cout << (dy - d1.value()) * 1e8 << std::endl;
+    std::cout << (dz - d1.value()) * 1e8 << std::endl;
+    */
+    double a(-64.347021769700607621);
+    double b(-64.019322340869338830);
+    double c(61.0133954162841680852);
+    adouble ad(a, 3, 0);
+    adouble bd(b, 3, 1);
+    adouble cd(c, 3, 2);
+    mpreal_wrapper<adouble> mad = mpreal_wrapper_type<adouble>::convert(ad);
+    mpfr::mpreal aa("-64.0193223408693388304", 63);
+    mad.value() = aa;
+    mpreal_wrapper<adouble> mbd = mpreal_wrapper_type<adouble>::convert(bd);
+    mbd.value().setPrecision(63);
+    mbd.value() = "-64.3470217697006076213";
+    mpreal_wrapper<adouble> mcd = mpreal_wrapper_type<adouble>::convert(cd);
+    mcd.value().setPrecision(63);
+    mcd.value() = "61.0133954162841680852";
+    std::cout << eintdiff<double>::run(b, a, c) << " " << eintdiff<mpfr::mpreal>::run(b, a, c) << std::endl ;
+    std::cout << eintdiff<mpreal_wrapper<adouble> >::run(mbd, mad, mcd) << std::endl;
 }
-*/
--- src/exponential_integrals.h
+++ src/exponential_integrals.h
@@ -9,42 +9,61 @@
 #include "gauss_legendre.h"
 
 mpfr::mpreal mpf_ei(const mpfr::mpreal&, const mp_prec_t);
-mpfr::mpreal mpf_ei(const mpfr::mpreal&, const mpfr::mpreal&, const mp_prec_t);
+mpfr::mpreal mpf_ei(const mpfr::mpreal&, bool, const mp_prec_t);
 
-template <typename T>
-T expintei(const T&, const T&);
-
+#ifdef EINTDIFF_QUAD
 template <typename T>
 T eint_helper(T x, void* r)
 {
     return exp(x + *(T*)r) / x;
 }
 
-#define EINTDIFF_QUAD 0
-#ifdef EINTDIFF_QUAD
 template <typename T>
-T eintdiff(const T a, const T b, T r)
+struct eintdiff
 {
-    // = e(r) * (eint(b) - eint(a));
-    // = -\int_a^b exp(t+r) / t dt
-    if (a > b)
-        return -eintdiff(b, a, r);
-    // T ret = exp(r) * (expintei(b) - expintei(a));
-    // check_nan(ret);
-    // return mpreal_wrapper_type<T>::convertBack(ret);
-    // std::function<T(const T, T*)> f(eint_helper<T>);
-    // return adaptiveSimpsons<T>(f, &r, a, b, 1e-8, 20);
-    T ret = gauss_legendre(64, eint_helper<T>, (void*)&r, a, b);
-    check_nan(ret);
-    return ret;
-}
+    static T run(const T a, const T b, const T r)
+    {
+        // = e(r) * (eint(b) - eint(a));
+        // = -\int_a^b exp(t+r) / t dt
+        if (a > b)
+            return -run(b, a, r);
+        // T ret = exp(r) * (expintei(b) - expintei(a));
+        // check_nan(ret);
+        // return mpreal_wrapper_type<T>::convertBack(ret);
+        // std::function<T(const T, T*)> f(eint_helper<T>);
+        // return adaptiveSimpsons<T>(f, &r, a, b, 1e-8, 20);
+        T ret = gauss_legendre(64, eint_helper<T>, (void*)&r, a, b);
+        check_nan(ret);
+        return ret;
+    }
+};
+
 #else
 template <typename T>
-T eintdiff(const T &a, const T &b, const T &r)
-{
-    return expintei(b, r) - expintei(a, r);
-}
-#endif
+T expintei(const T&, const T&);
 
+template <typename T>
+struct eintdiff
+{
+    static T run(const T &a, const T &b, const T &r)
+    {
+        return expintei(b, r) - expintei(a, r);
+    }
+};
+
+template <typename T>
+struct eintdiff<Eigen::AutoDiffScalar<T> >
+{
+    static Eigen::AutoDiffScalar<T> run(const Eigen::AutoDiffScalar<T> &a, 
+            const Eigen::AutoDiffScalar<T> &b, const Eigen::AutoDiffScalar<T> &c)
+    {
+        Eigen::AutoDiffScalar<T> ret;
+        ret.value() = eintdiff<typename Eigen::AutoDiffScalar<T>::Scalar>::run(a.value(), b.value(), c.value());
+        ret.derivatives() = exp(c.value()) * (exp(b.value()) / b.value() * b.derivatives() - exp(a.value()) / a.value() * a.derivatives());
+        ret.derivatives() += c.derivatives() * ret.value();
+        return ret;
+    }
+};
+#endif
 
 #endif
