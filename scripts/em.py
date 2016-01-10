8d738b6f480f9895ca4fea7ffcf9546ab7b83b33
Jonathan Terhorst <terhorst@gmail.com>
2016-01-10 12:27:53 -0800

    updates to EM script and configuration

--- scripts/em.py
+++ scripts/em.py
@@ -1,5 +1,5 @@
 #/usr/bin/env python2.7
-# Complete example showing how to use the package for inference
+'''Fit SMC++ model to data using the EM algorithm.'''
 from __future__ import division
 import numpy as np
 import scipy.optimize
@@ -7,18 +7,30 @@ import pprint
 import multiprocessing
 import sys
 import itertools
-from collections import Counter
 import sys
 import time
-import argparse
-import psmcpp._pypsmcpp
-from psmcpp.lib.util import config2dict
-import ConfigParser as configparser
-import cPickle as pickle
+import configargparse
+import logging
+import os
 
-t_start = time.time()
+# Package imports
+import psmcpp._pypsmcpp, psmcpp.lib.util
 
-np.set_printoptions(linewidth=120, precision=6, suppress=False)
+np.set_printoptions(linewidth=120, precision=6, suppress=True)
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+
+## Helper functions used for multiprocessing
+def _norm_help(args):
+    logger.info("Normalizing dataset...")
+    return psmcpp.lib.util.normalize_dataset(*args)
+
+def _obsfs_helper(ol):
+    obsfs = np.zeros([3, n - 1])
+    olsub = ol[np.logical_and(ol[:, 1:3].min(axis=1) != -1, ol[:, -1] == n - 2)]
+    for r, c1, c2, _ in olsub:
+        obsfs[c1, c2] += r
+    return obsfs
 
 def exp_quantiles(M, h_M):
     hs = -np.log(1. - np.linspace(0, h_M, M, False) / h_M)
@@ -26,152 +38,136 @@ def exp_quantiles(M, h_M):
     hs[0] = 0
     return hs
 
-parser = argparse.ArgumentParser("smc++")
-parser.add_argument("--debug", action="store_true", default=False, help="display a lot of debugging info")
-parser.add_argument("--output", "-o", type=argparse.FileType('wt'), help="output file", default=sys.stdout)
-parser.add_argument('config', type=argparse.FileType('r'), help="config file")
-parser.add_argument('data', nargs="+", help="data file in SMC++ format")
+## Configure argument parser
+parser = configargparse.ArgumentParser("SMC++")
+pop_params = parser.add_argument_group('population parameters')
+model = parser.add_argument_group('model')
+hmm = parser.add_argument_group('HMM and fitting parameters')
+parser.add_argument("--output-directory", "-o", help="output directory", default=".")
+parser.add_argument('--config', is_config_file=True, help="config file path")
+pop_params.add_argument('--N0', type=float, help="reference effective population size", required=True)
+pop_params.add_argument('--mu', type=float, help="per-generation mutation rate", required=True)
+pop_params.add_argument('--rho', type=float, help="per-generation recombination rate", required=True)
+model.add_argument('--pieces', type=str, help="span of model pieces", required=True, default="32*1")
+model.add_argument('--t1', type=float, help="end-point of first piece, in generations", required=True, default=40.)
+model.add_argument('--tK', type=float, help="end-point of last piece, in generations", required=True, default=40000.)
+model.add_argument('--exponential-pieces', nargs="+", type=int, action="append", help="pieces which have exponential growth")
+hmm.add_argument('--thinning', type=int, help="frequency to emit full SFS (default: 25 * sample size)")
+hmm.add_argument('--M', type=int, help="number of hidden states", required=True, default=32)
+hmm.add_argument('--hM', type=float, help="hidden state cutoff, in generations", required=True, default=8000.)
+hmm.add_argument('--em-iterations', type=float, help="number of EM steps to perform", default=20)
+hmm.add_argument('--lambda-penalty', type=float, help="regularization penalty", default=.01)
+hmm.add_argument('--lbfgs-factor', type=float, help="stopping criterion for optimizer", default=1e10)
+hmm.add_argument('--Nmin', type=float, help="Lower bound on effective population size", default=500)
+hmm.add_argument('--Nmax', type=float, help="Upper bound on effective population size", default=100000)
+parser.add_argument('data', nargs="+", help="data file(s) in SMC++ format")
 args = parser.parse_args()
 
-psmcpp._pypsmcpp.do_progress(args.debug)
+## Create output directory and dump all values
+try:
+    os.makedirs(args.output_directory)
+except OSError:
+    pass # directory exists
+parser.print_values(open(os.path.join(args.output_directory, "config.txt"), "wt"))
+
+## Begin main script
+## Step 1: load data and clean up a bit
 try:
     smcpp_data = pickle.load(open(args.data[0], "rb"))
 except:
     smcpp_data = psmcpp.lib.util.parse_text_datasets(args.data)
-obs_list = smcpp_data['obs']
-print(len(obs_list))
 n = smcpp_data['n']
-config = configparser.SafeConfigParser()
-config.readfp(args.config)
-print(config2dict(config))
+
+if args.thinning is None:
+    args.thinning = 25 * n
+
+pool = multiprocessing.Pool(None)
+obs_list = list(pool.map(_norm_help, [(ob, args.thinning) for ob in smcpp_data['obs']]))
 
 ## Calculate observed SFS for use later
-def _obsfs_helper(ol):
-    obsfs = np.zeros([3, n - 1])
-    olsub = ol[np.logical_and(ol[:, 1:3].min(axis=1) != -1, ol[:, -1] == n - 2)]
-    for r, c1, c2, _ in olsub:
-        obsfs[c1, c2] += r
-    return obsfs
-pool = multiprocessing.Pool(None)
 osfs = list(pool.map(_obsfs_helper, obs_list))
 pool.close()
 pool.terminate()
 pool = None
 obsfs = np.sum(osfs, axis=0)
 obsfs /= obsfs.sum()
-print(" - Observed sfs:")
-print(obsfs)
+logger.info("Observed SFS:\n%s", str(obsfs))
 
-try:
-    ts = np.array(eval(config.get('model', 'ts')))
-    s = ts[1:] - ts[:-1]
-    K = len(s)
-except configparser.NoOptionError:
-    t_1 = config.getfloat('model', 't_1')
-    t_K = config.getfloat('model', 't_K')
-    Ks = config.get('model', 'K').split("+")
-    pieces = []
-    for piece in Ks:
-        try:
-            num, span = list(map(int, piece.split("*")))
-        except ValueError:
-            span = int(piece)
-            num = 1
-        pieces += [span] * num
-    s = np.logspace(np.log10(t_1), np.log10(t_K), sum(pieces))
-    s = np.concatenate(([t_1], s[1:] - s[:-1]))
-    sp = np.zeros(len(pieces))
-    count = 0
-    for i, p in enumerate(pieces):
-        sp[i] = s[count:(count+p)].sum()
-        count += p
-    s = sp
-print("time points", s)
-print(np.cumsum(s))
+pieces = []
+for piece in args.pieces.split("+"):
+    try:
+        num, span = list(map(int, piece.split("*")))
+    except ValueError:
+        span = int(piece)
+        num = 1
+    pieces += [span] * num
+args.t1 /= 2 * args.N0
+args.tK /= 2 * args.N0
+
+s = np.logspace(np.log10(args.t1), np.log10(args.tK), sum(pieces))
+s = np.concatenate(([args.t1], s[1:] - s[:-1]))
+sp = np.zeros(len(pieces))
+count = 0
+for i, p in enumerate(pieces):
+    sp[i] = s[count:(count+p)].sum()
+    count += p
+s = sp
+mp = {'s': s, 'K': len(s)}
+logger.debug("time points in coalescent scaling:\n%s", str(s))
+
+## Initialize model values
+mp['x'] = np.ones([2, mp['K']])
+mp['a'] = mp['x'][0]
+mp['b'] = mp['x'][1]
+flat_pieces = [i for i in range(mp['K']) if i not in args.exponential_pieces]
+mp['b'][:] = mp['a'] + 0.1
+mp['b'][flat_pieces] = mp['a'][flat_pieces]
 
 ## Compute hidden states
-try:
-    hs = np.array(eval(config.get('hidden states', 'hidden states')))
-except configparser.NoOptionError:
-    M = config.getint('hidden states', 'M')
-    h_M = config.getfloat('hidden states', 'h_M')
-    hs = exp_quantiles(M, h_M)
+args.hM /= 2 * args.N0
+hs = exp_quantiles(args.M, args.hM)
 if hs[0] != 0:
     raise Exception("First hidden state interval must begin at 0")
-hs = np.unique(np.sort(np.concatenate([hs, np.cumsum(s)])))
-print("hidden states", hs)
+mp['hidden states'] = np.unique(np.sort(np.concatenate([hs, np.cumsum(s)])))
+logging.debug("hidden states:\n%s", str(mp['hidden states']))
 
-# Load additional params
-N0 = config.getfloat('parameters', 'N0')
-mu = config.getfloat('parameters', 'mu')
-rho = config.getfloat('parameters', 'rho')
-
-try:
-    lambda_penalty = config.getfloat("advanced", "lambda penalty")
-except configparser.NoOptionError:
-    lambda_penalty = 0.0
-
-im = psmcpp._pypsmcpp.PyInferenceManager(n - 2, obs_list, hs, 4.0 * N0 * mu, 4.0 * N0 * rho)
-
-K = len(s)
-x0 = np.ones([2, K])
-a, b = x0
-try:
-    exponential_pieces = eval(config.get('model', 'exponential pieces'))
-except configparser.NoOptionError:
-    exponential_pieces = []
-flat_pieces = [i for i in range(K) if i not in exponential_pieces]
-b[:] = a + 0.1
-b[flat_pieces] = a[flat_pieces]
-
-im.setParams((a,b,s),False)
+## Create inference object which will be used for all further calculations.
+im = psmcpp._pypsmcpp.PyInferenceManager(n - 2, obs_list, mp['hidden states'], 2.0 * args.N0 * args.mu, 2.0 * args.N0 * args.rho)
+im.setParams([mp[x] for x in "abs"], False)
 im.Estep()
 
 llold = -np.inf
-bounds = np.array([[0.1, 20.0]] * K + [[0.15, 19.9]] * K).reshape([2, K, 2])
 
-# Optimization part
-precond = 1. / s
+## Bounds
+args.Nmin /= 2 * args.N0
+args.Nmax /= 2 * args.N0
+bounds = np.array([[args.Nmin, args.Nmax]] * mp['K'] + [[1.01 * args.Nmin, 0.99 * args.Nmax]] * mp['K']).reshape([2, mp['K'], 2])
+
+## Optimization stuff 
+# Vector of "preconditioners" helps with optimization
+precond = 1. / mp['s']
 precond[-1] = 1. / (15.0 - np.sum(s))
 
 def optimize_fullgrad(iter, coords, x0, factr=1e9):
-    print("Optimizing factr {factr}".format(factr=factr))
+    logger.debug("Optimizing factr {factr}".format(factr=factr))
     def fprime(x):
-        x0c = x0.copy()
+        x0c = mp['x'].copy()
         # Preconditioner (sort of)
         for xx, cc in zip(x, coords):
             x0c[cc] = xx * precond[cc[1]]
-        global s
         aa, bb = x0c
         bb[flat_pieces] = aa[flat_pieces]
-        print(aa)
-        print(bb)
-        print(s)
-        im.setParams((aa, bb, s), coords)
-        print("done")
-        res = im.Q(lambda_penalty)
+        im.setParams((aa, bb, mp['s']), coords)
+        res = im.Q(args.lambda_penalty)
         lls = np.array([ll for ll, jac in res])
         jacs = np.array([jac for ll, jac in res])
         ret = [-np.mean(lls, axis=0), -np.mean(jacs, axis=0)]
-        dary = np.zeros([2, K])
+        dary = np.zeros([2, mp['K']])
         for i, cc in enumerate(coords):
             ret[1][i] *= precond[cc[1]]
             dary[cc] = ret[1][i]
-        print(dary)
-        print(ret[0])
-        # reg = im.regularizer()
-        # print("regularizer: ", LAMBDA_PENALTY * reg)
-        # add penalty
-        # esfs, jac = psmcpp._pypsmcpp.sfs(n, (aa, bb, s), 0.0, hs1[-1], 4 * N0 * theta, coords)
-        # diff = esfs[0, 0] - obsfs[0, 0]
-        # penalty = ALPHA_PENALTY * diff**2
-        # print("penalty", penalty)
-        # ret[0] += penalty
-        # ret[1] += 2 * ALPHA_PENALTY * diff * jac[0, 0]
-        # print(x)
-        # print(ret[0])
-        # print(ret[1])
-        # print("------")
+        logging.debug(dary)
+        logging.debug(ret[0])
         return ret
     # print("gradient check")
     # xx0 = np.array([x0[cc] / precond[cc[1]] for cc in coords])
@@ -181,9 +177,9 @@ def optimize_fullgrad(iter, coords, x0, factr=1e9):
     #     x0c[i] += 1e-8
     #     f1, _ = fprime(x0c)
     #     print(i, cc, f1, f0, (f1 - f0) / 1e-8, fp[i])
-    res = scipy.optimize.fmin_l_bfgs_b(fprime, [x0[cc] / precond[cc[1]] for cc in coords], 
+    res = scipy.optimize.fmin_l_bfgs_b(fprime, [mp['x'][cc] / precond[cc[1]] for cc in coords], 
             None, bounds=[tuple(bounds[cc] / precond[cc[1]]) for cc in coords], disp=False, factr=factr)
-    # print(res)
+    logging.debug(res)
     return np.array([x * precond[cc[1]] for x, cc in zip(res[0], coords)])
 
 break_loop = False
@@ -205,27 +201,17 @@ def reverse_progress(signal, frame):
 signal.signal(signal.SIGUSR1, reverse_progress)
 
 def run_iteration(i, coords, factr):
-    global a
-    global b
-    global x0
-    global llold
-    global im
-    global hs
     # for j in range(K * di // 3, K * (di + 1) // 3)]
-    ret = optimize_fullgrad(i, coords, x0, factr)
+    ret = optimize_fullgrad(i, coords, mp['x'], factr)
     for xx, cc in zip(ret, coords):
-        x0[cc] = xx
-    print(x0)
-    print(list(zip(ret, coords)))
-    print(flat_pieces)
-    b[flat_pieces] = a[flat_pieces]
-    print("************** ITERATION %d ***************" % i)
-    print(a)
-    print(b)
+        mp['x'][cc] = xx
+    mp['b'][flat_pieces] = mp['a'][flat_pieces]
+    logging.info("************** EM ITERATION %d ***************" % i)
+    logging.info("Current model:\n%s", str(mp['x']))
     if i == 5:
         print("rebalancing hidden states")
         h_M = hs[-1]
-        hs = im.balance_hidden_states((a, b, s), M)
+        hs = im.balance_hidden_states((mp['a'], mp['b'], mp['s']), M)
         hs[-1] = h_M
         hs = np.unique(np.sort(np.concatenate([hs, np.cumsum(s)])))
         im.hidden_states = hs
@@ -245,23 +231,13 @@ def run_iteration(i, coords, factr):
     return ret
 
 i = 0
-coords = [(aa, j) for j in range(K) for aa in ((0,) if j in flat_pieces else (0, 1))]
-try:
-    iterations = config.getint("advanced", "em iterations")
-except configparser.NoOptionError:
-    iterations = 20
-try:
-    factr = config.getfloat("advanced", "lbfgs factor")
-except configparser.NoOptionError:
-    factr = 1e9
+coords = [(aa, j) for j in range(mp['K']) for aa in ((0,) if j in flat_pieces else (0, 1))]
 
-while i < iterations:
-    run_iteration(i, coords, factr)
-    esfs = psmcpp._pypsmcpp.sfs(n, (a,b,s), 0.0, hs[-1], 4 * N0 * mu, False)
-    print("calculated sfs")
-    print(esfs)
-    print("observed sfs")
-    print(obsfs)
+while i < args.em_iterations:
+    run_iteration(i, coords, args.lbfgs_factor)
+    esfs = psmcpp._pypsmcpp.sfs(n, (mp['a'],mp['b'],mp['s']), 0.0, mp['hidden states'][-1], 2 * N0 * args.mu, False)
+    logging.debug("model sfs:\n%s" % str(esfs))
+    logging.debug("observed sfs:\n%s" % str(esfs))
     i += 1
 # Output state and exit
 d = print_state()
