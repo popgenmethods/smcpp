c420a7f447509e74519d1a3a0706bdff89f30709
Jonathan Terhorst <terhorst@gmail.com>
2016-07-17 14:37:37 -0700

    more support for jcsfs

--- smcpp/jcsfs.py
+++ smcpp/jcsfs.py
@@ -5,7 +5,27 @@ import numpy as np
 import multiprocessing as mp
 import ctypes
 
-from . import model, logging, _smcpp, pool
+from . import model, logging, _smcpp
+from .moran_eigensystem import MoranEigensystem
+
+import time
+
+class PiecewiseModel(object):
+    def __init__(self, s, a, dlist):
+        self.s = s
+        self.a = a
+        self.dlist = dlist
+    def stepwise_values(self):
+        return self.a
+
+class Timer:    
+    def __enter__(self):
+        self.start = time.clock()
+        return self
+
+    def __exit__(self, *args):
+        self.end = time.clock()
+        print("took %g" % (self.end - self.start,))
 
 logger = logging.getLogger(__name__)
 
@@ -32,10 +52,10 @@ class JointCSFS(object):
         self._split = split
         self._hs = hidden_states
         ## FIXME this is hacky. should be replaced by calls to eta, probably
-        self._Rts1 = float(_R(self._model1, split))
-        self._Rts2 = float(_R(self._model2, split))
-        self._jcsfs = np.zeros([len(hidden_states) - 1, self._a1 + 1, self._n1 + 1, self._a2 + 1, self._n2 + 1], dtype=float)
-        self._hs_pairs = zip(hidden_states[:-1], hidden_states[1:])
+        self._Rts1 = _R(self._model1, split)
+        self._Rts2 = _R(self._model2, split)
+        self._jcsfs = np.zeros([len(hidden_states) - 1, self._a1 + 1, self._n1 + 1, self._a2 + 1, self._n2 + 1], dtype=object)
+        self._hs_pairs = list(zip(hidden_states[:-1], hidden_states[1:]))
         for t1, t2 in self._hs_pairs:
             assert (t1 < t2 <= split) or (split <= t1 < t2)
         assert a1 + a2 == 2
@@ -44,25 +64,19 @@ class JointCSFS(object):
         assert a2 == 0, "distinguished lineages originating in different populations not currently supported"
 
     def compute(self):
+        ctx = mp.get_context("spawn")
+        p = ctx.Pool()
         args = tuple([getattr(self, "_" + x) for x in "split, a1, a2, n1, n2, model1, model2, Rts1, Rts2, K".split(", ")])
-        processes = []
-        ary = mp.Array('d', self._jcsfs.size)
-        for i, (t1, t2) in enumerate(self._hs_pairs):
-            processes.append(mp.Process(target=_parallel_helper, args=(ary, self._jcsfs.shape, i, t1, t2) + args))
-            processes[-1].start()
-        for p in processes:
-            p.join()
-        # self._jcsfs[:] = list(p.imap(_parallel_helper, ((t1, t2) + args for t1, t2 in self._hs_pairs), 1))
+        self._jcsfs[:] = list(map(_parallel_helper, ((t1, t2) + args for t1, t2 in self._hs_pairs)))
         return self._jcsfs
 
-def _parallel_helper(ary, shape, i, t1, t2, split, *args):
-    jcsfs = np.ctypeslib.as_array(ary.get_obj())
-    jcsfs.shape = shape
+def _parallel_helper(tup):
+    t1, t2, split, *args = tup
     print(t1, t2)
     if t1 < t2 <= split:
-        jcsfs[i] = _jcsfs_helper_tau_below_split(t1, t2, split, *args)
+        return _jcsfs_helper_tau_below_split(t1, t2, split, *args)
     elif split <= t1 < t2:
-        jcsfs[i] = _jcsfs_helper_tau_above_split(t1, t2, split, *args)
+        return _jcsfs_helper_tau_above_split(t1, t2, split, *args)
     return
 
 
@@ -74,29 +88,30 @@ def _jcsfs_helper_tau_above_split(t1, t2, split, a1, a2, n1, n2, model1, model2,
     # Two parts: branch length between split and tau, and branch
     # length above tau.
     #
-    s = np.array(model1.s, dtype=float)
-    a = np.array(model1.stepwise_values(), dtype=float)
+    s = model1.s
+    a = model1.stepwise_values()
     cs = _cumsum0(s)
     i = np.searchsorted(cs, split)
     cs[i - 1] = split
     sp = np.diff(cs[i - 1:])
     ap = a[i - 1:]
-    shim_model = model.PiecewiseModel(sp, ap)
-    rsfs = _smcpp.raw_sfs(shim_model, n1 + n2 + 2, t1, t2, True)
-    M0 = _modified_rate_matrix(n1 + n2, 0)
+    dlist = set(model1.dlist) & {a.d().keys() for a in ap if hasattr(a, 'd')}
+    shim_model = PiecewiseModel(sp, ap, list(dlist))
+    rsfs = _smcpp.raw_sfs(shim_model, n1 + n2 + 2, t1, t2, True).astype('object')
     S2 = np.arange(1, n1 + n2 + 1, dtype=float) / (n1 + n2 + 1)
     S0 = 1. - S2
-    M10 = _modified_rate_matrix(n1, 0)
-    M11 = _modified_rate_matrix(n1, 1)
-    M21 = _modified_rate_matrix(n1, 2)
-    eM10 = scipy.linalg.expm(Rts1 * M10)
-    eM11 = scipy.linalg.expm(Rts1 * M11)
-    eM21 = scipy.linalg.expm(Rts1 * M21)
-    M2 = _moran_rate_matrix(n2)
-    eM2 = scipy.linalg.expm(Rts2 * M2)
+    M0 = MoranEigensystem(n1 + n2, 0)
+    M10 = MoranEigensystem(n1, 0)
+    M11 = MoranEigensystem(n1, 1)
+    eM10 = M10.expm(Rts1)
+    eM11 = M11.expm(Rts1)
+    eM12 = eM10[::-1, ::-1]
+    M2 = MoranEigensystem(n2)
+    eM2 = M2.expm(Rts2)
     # add additional time above tau by numerical integration
     # sample tau, compute sfs at tau, then moran transition to t_s, 
     configs = [((x, n1 + n2 + 1 - x),) for x in range(1, n1 + n2 + 1)]
+
     for tau, Rtau in _smcpp.random_coal_times(model1, t1, t2, K):
         logger.debug((t1, t2, tau, Rtau))
         # Compute regular old sfs using momi
@@ -104,7 +119,7 @@ def _jcsfs_helper_tau_above_split(t1, t2, split, a1, a2, n1, n2, model1, model2,
         tm1 = np.searchsorted(m1s, tau) - 1
         m1s[tm1] = tau
         m1s = m1s[tm1:]
-        a = model1.stepwise_values()[tm1:].astype('float')
+        m1a = model1.stepwise_values()[tm1:]
         # "shift time back" by t
         m1s -= tau
         events = _model_to_momi_events(m1s, a, "pop1")
@@ -114,12 +129,13 @@ def _jcsfs_helper_tau_above_split(t1, t2, split, a1, a2, n1, n2, model1, model2,
         # esfs is esfs at tau. first compute transition down to t_s.
         # there are two cases to consider depending on whether a mutation
         # hits distinguished lineages.
-        eM0 = scipy.linalg.expm((Rtau - Rts1) * M0)
+        eM0 = M0.expm(Rtau - Rts1)
         eM2 = eM0[::-1, ::-1]
         sfs0_ts = (esfs * S0).dot(eM0[1:])
         sfs2_ts = (esfs * S2).dot(eM2[:-1])
         rsfs[0] += sfs0_ts / K
         rsfs[2] += sfs2_ts / K
+        assert(np.all(rsfs >= 0))
 
     # Moran transition in each subpopulation from t_S to present
     for b1 in range(n1 + 1):
@@ -139,7 +155,7 @@ def _jcsfs_helper_tau_above_split(t1, t2, split, a1, a2, n1, n2, model1, model2,
                             )
                     ret[2, b1, 0, b2] += (
                             h * rsfs[2, nseg] *
-                            eM21[np1, b1] * eM2[np2, b2]
+                            eM12[np1, b1] * eM2[np2, b2]
                             )
     return ret
 
@@ -150,14 +166,13 @@ def _jcsfs_helper_tau_below_split(t1, t2, split, a1, a2, n1, n2, model1, model2,
     ret = np.zeros([a1 + 1, n1 + 1, a2 + 1, n2 + 1])
     S2 = np.diag(np.arange(1, n1 + 1, dtype=float)) / (n1 + 1)
     S0 = np.eye(n1) - S2
-    M0 = _modified_rate_matrix(n1, 0)
+    M0 = MoranEigensystem(n1, 0)
     rsfs = _smcpp.raw_sfs(model1, n1 + 2, t1, t2, True)
     ret[:, :, 0, 0] = rsfs
     m2s = _cumsum0(model2.s)
     tm2 = np.searchsorted(m2s, split)
     m2s = m2s[:tm2]
-    m2a = model2.stepwise_values()[:tm2].astype('float')
-    p2_events = [("-en", tt, "pop2", aa) for tt, aa in zip(m2s, m2a)]
+    m2a = model2.stepwise_values()[:tm2]
     configs = [((b1, n1 + 1 - b1), (b2, n2 - b2))
                for b1 in range(n1 + 2)
                for b2 in range(n2 + 1)
@@ -178,7 +193,7 @@ def _jcsfs_helper_tau_below_split(t1, t2, split, a1, a2, n1, n2, model1, model2,
         sfs = np.zeros([n1 + 2, n2 + 1])
         esfs = momi.expected_sfs(demo, mconf, mut_rate=1.0)
         sfs.flat[1:-1] = esfs
-        trans0 = scipy.linalg.expm(Rt * M0)
+        trans0 = M0.expm(Rt)
         trans2 = trans0[::-1, ::-1]
         ret[0, 0, 0] += sfs[0] / K
         ret[2, n1, 0] += sfs[-1] / K
