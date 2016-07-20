87985a58adc6f2a5f57e65f419fb2163a3e457d5
Jonathan Terhorst <terhorst@gmail.com>
2016-07-20 10:15:04 -0700

    autopep8

--- smcpp/commands/estimate.py
+++ smcpp/commands/estimate.py
@@ -18,97 +18,106 @@ logger = getLogger(__name__)
 
 from .. import _smcpp, util, estimation_tools
 from ..model import SMCModel
-from ..population import Population
-from ..optimizer import PopulationOptimizer
+from ..analysis import Analysis
+from ..optimizer import SMCPPOptimizer
 from ..logging import init_logging
 
 np.set_printoptions(linewidth=120, suppress=True)
 
+
 def init_parser(parser):
     '''Configure parser and parse args.'''
     # FIXME: argument groups not supported in subparsers
-    pop_params = parser # parser.add_argument_group('population parameters')
-    model = parser # parser.add_argument_group('model')
-    hmm = parser # parser.add_argument_group('HMM and fitting parameters')
+    pop_params = parser  # parser.add_argument_group('population parameters')
+    model = parser  # parser.add_argument_group('model')
+    hmm = parser  # parser.add_argument_group('HMM and fitting parameters')
     optimizer = parser
-    model.add_argument('--pieces', type=str, help="span of model pieces", default="32*1")
-    model.add_argument('--t1', type=float, nargs="+", help="span of first piece(s), in generations", default=[400.])
-    model.add_argument('--tK', type=float, help="end-point of last piece, in generations", default=40000.)
-    model.add_argument('--exponential-pieces', type=int, nargs="+", help="piece(s) which have exponential growth")
-    hmm.add_argument('--thinning', help="emit full SFS every <k>th site", default=None, type=int, metavar="k")
-    hmm.add_argument('--no-pretrain', help="do not pretrain model", action="store_true", default=False)
-    hmm.add_argument('--M', type=int, help="number of hidden states", default=32)
-    hmm.add_argument('--em-iterations', type=int, help="number of EM steps to perform", default=20)
-    optimizer.add_argument('--blocks', type=int, default=1, help="number of blocks for coordinate ascent")
-    optimizer.add_argument('--regularization-penalty', type=float, help="regularization penalty", default=None)
-    optimizer.add_argument('--pretrain-penalty', type=float, help="regularization penalty for pretraining", default=1e-7)
-    optimizer.add_argument('--regularizer', 
-            choices=[x + y for x in ["", "log"] for y in ["abs", "quadratic", "curvature"]],
-            default="quadratic", help="type of regularization to apply")
+    model.add_argument('--pieces', type=str,
+                       help="span of model pieces", default="32*1")
+    model.add_argument('--t1', type=float, nargs="+",
+                       help="span of first piece(s), in generations", default=[400.])
+    model.add_argument(
+        '--tK', type=float, help="end-point of last piece, in generations", default=40000.)
+    model.add_argument('--exponential-pieces', type=int,
+                       nargs="+", help="piece(s) which have exponential growth")
+    hmm.add_argument('--thinning', help="emit full SFS every <k>th site",
+                     default=None, type=int, metavar="k")
+    hmm.add_argument('--no-pretrain', help="do not pretrain model",
+                     action="store_true", default=False)
+    hmm.add_argument(
+        '--M', type=int, help="number of hidden states", default=32)
+    hmm.add_argument('--em-iterations', type=int,
+                     help="number of EM steps to perform", default=20)
+    optimizer.add_argument('--block-size', type=int, default=3,
+                           help="number of blocks to optimizer at a time for coordinate ascent")
+    optimizer.add_argument('--regularization-penalty',
+                           type=float, help="regularization penalty", default=None)
+    optimizer.add_argument('--pretrain-penalty', type=float,
+                           help="regularization penalty for pretraining", default=1e-7)
+    optimizer.add_argument('--regularizer',
+                           choices=[x + y for x in ["", "log"]
+                                    for y in ["abs", "quadratic", "curvature"]],
+                           default="quadratic", help="type of regularization to apply")
     optimizer.add_argument('--spline',
-            choices=["cubic", "akima", "pchip"],
-            default="pchip", help="type of spline representation to use")
-    optimizer.add_argument('--lbfgs-factor', type=float, help="stopping criterion for optimizer", default=1e9)
-    optimizer.add_argument('--Nmin', type=float, help="Lower bound on effective population size (in units of N0)", default=.01)
-    optimizer.add_argument('--Nmax', type=float, help="Upper bound on effective population size (in units of N0)", default=100)
-    hmm.add_argument('--init-model', type=str, help="model to use as starting point in optimization")
+                           choices=["cubic", "akima", "pchip"],
+                           default="pchip", help="type of spline representation to use")
+    optimizer.add_argument('--lbfgs-factor', type=float,
+                           help="stopping criterion for optimizer", default=1e9)
+    optimizer.add_argument(
+        '--Nmin', type=float, help="Lower bound on effective population size (in units of N0)", default=.01)
+    optimizer.add_argument(
+        '--Nmax', type=float, help="Upper bound on effective population size (in units of N0)", default=100)
+    hmm.add_argument('--init-model', type=str,
+                     help="model to use as starting point in optimization")
     hmm.add_argument('--hidden-states', type=float, nargs="+")
-    pop_params.add_argument('--N0', default=1e4, type=float, help="reference effective (diploid) population size to scale output.")
-    pop_params.add_argument('--theta', type=float, 
-            help="population-scaled mutation rate. default: watterson's estimator.")
-    pop_params.add_argument('--rho', type=float, 
-            help="population-scaled mutation rate. default: theta.")
-    pop_params.add_argument("--fix-rho", default=False, action="store_true", 
-            help="do not estimate recombination rate from data")
+    pop_params.add_argument('--N0', default=1e4, type=float,
+                            help="reference effective (diploid) population size to scale output.")
+    pop_params.add_argument('--theta', type=float,
+                            help="population-scaled mutation rate. default: watterson's estimator.")
+    pop_params.add_argument('--rho', type=float,
+                            help="population-scaled mutation rate. default: theta.")
+    pop_params.add_argument("--fix-rho", default=False, action="store_true",
+                            help="do not estimate recombination rate from data")
     pop_params.add_argument("--folded", action="store_true", default=False,
-            help="use folded SFS for emission probabilites. useful if polarization is not known.")
-    hmm.add_argument('--length-cutoff', help="omit sequences < cutoff", default=0, type=int)
-    parser.add_argument("-p", "--second-population", nargs="+", widget="MultiFileChooser", 
-            help="Estimate divergence time using data set(s) from a second subpopulation")
-    parser.add_argument("-o", "--outdir", help="output directory", default=".", widget="DirChooser")
-    parser.add_argument('-v', '--verbose', action='count', help="increase debugging output, specify multiply times for more")
-    parser.add_argument('data', nargs="+", help="data file(s) in SMC++ format", widget="MultiFileChooser")
+                            help="use folded SFS for emission probabilites. useful if polarization is not known.")
+    hmm.add_argument('--length-cutoff',
+                     help="omit sequences < cutoff", default=0, type=int)
+    parser.add_argument("-p", "--second-population", nargs="+", widget="MultiFileChooser",
+                        help="Estimate divergence time using data set(s) from a second subpopulation")
+    parser.add_argument("-o", "--outdir", help="output directory",
+                        default=".", widget="DirChooser")
+    parser.add_argument('-v', '--verbose', action='count',
+                        help="increase debugging output, specify multiply times for more")
+    parser.add_argument(
+        'data', nargs="+", help="data file(s) in SMC++ format", widget="MultiFileChooser")
+
 
 def validate_args(args):
     # perform some sanity checking on the args
     if args.theta is not None:
         pgm = args.theta / (2. * args.N0)
         if not (1e-12 <= pgm <= 1e-2):
-            logger.warn("The per-generation mutation rate is calculated to be %g. Is this correct?" % pgm)
+            logger.warn(
+                "The per-generation mutation rate is calculated to be %g. Is this correct?" % pgm)
+
 
 def main(args):
-    'Main control loop for EM algorithm'
     ## Create output directory
     try:
         os.makedirs(args.outdir)
     except OSError:
-        pass # directory exists
+        pass  # directory exists
 
     ## Initialize the logger
-    init_logging(args.outdir, args.verbose, os.path.join(args.outdir, ".debug.txt"))
+    init_logging(args.outdir, args.verbose,
+                 os.path.join(args.outdir, ".debug.txt"))
     ## Save all the command line args and stuff
     logger.debug(sys.argv)
     logger.debug(args)
-    
+
     ## Perform some validation on the arguments
     validate_args(args)
 
-    ## Begin main script
-    ## Step 1: load data and clean up a bit
-    datasets_files = [args.data]
-    if args.second_population:
-        logger.info("Split estimation mode selected")
-        datasets_files.append(args.second_population)
-    
-    ## Construct populations
-    populations = [Population(dsf, args) for dsf in datasets_files]
-
-    npop = len(populations)
-    if npop == 1:
-        opt_klass = PopulationOptimizer
-    else:
-        raise RuntimeError("> 1 population not currently supported")
-    opt = opt_klass(populations[0], args.outdir, args.regularization_penalty)
-
-    # Run the optimizer
-    opt.run(args.em_iterations, args.blocks, args.fix_rho)
+    ## Construct analysis
+    analysis = Analysis(args.data, args)
+    analysis.run()
