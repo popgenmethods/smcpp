99743d46db1216b8ed8a98efb9c6e9a4d34f8d64
Jonathan Terhorst <terhorst@gmail.com>
2016-07-25 21:21:50 -0700

    remove pool thing

--- test/unit/fixtures.py
+++ test/unit/fixtures.py
@@ -1,10 +1,8 @@
 import pytest
 import numpy as np
 
-import smcpp.pool
-smcpp.pool.init_pool()
-
 import smcpp._smcpp, smcpp.model
+
 
 @pytest.fixture
 def im():
@@ -16,7 +14,9 @@ def im():
     n = 26
     M = len(hidden_states) - 1
     s = np.diff(np.logspace(np.log10(.01), np.log10(3.), 41))
-    obs_list = [np.array([[1, 0, 10, n - 2], [10, 0, 0, n - 2], [1, 2, 2, n - 5]], dtype=np.int32)]
+    obs_list = [np.array(
+        [[1, 0, 10, n - 2], [10, 0, 0, n - 2], [1, 2, 2, n - 5]],
+        dtype=np.int32)]
     im = smcpp._smcpp.PyOnePopInferenceManager(n - 2, obs_list, hidden_states)
     im.rho = 4.0 * N0 * rho
     im.theta = 4.0 * N0 * theta
