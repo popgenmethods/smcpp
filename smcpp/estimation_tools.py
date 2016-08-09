98bf67858d598d92e04aacb218161220269ca914
Jonathan Terhorst <terhorst@gmail.com>
2016-08-09 12:58:58 -0700

    pep8 cleanups

--- smcpp/estimation_tools.py
+++ smcpp/estimation_tools.py
@@ -1,19 +1,55 @@
 'Miscellaneous estimation and data-massaging functions.'
 from __future__ import absolute_import, division, print_function
-import sys
 import numpy as np
 from logging import getLogger
-logger = getLogger(__name__)
 import scipy.optimize
 import scipy.interpolate
 import multiprocessing as mp
-import ad.admath, ad.linalg
+from collections import namedtuple
+import contextlib
 
 from . import _smcpp
 
-## 
-## Construct time intervals stuff
-## 
+logger = getLogger(__name__)
+
+
+@contextlib.contextmanager
+def mp_pool():
+    # Safely destroy multiprocessing pool to avoid annoying stack traces
+    # on KeyboardInterrupt
+    p = mp.Pool()
+    yield p
+    p.close()
+    p.join()
+    p.terminate()
+
+
+# Simple adagrad implementation with bounds.
+AdagradResult = namedtuple("AdagradResult", "x f")
+
+
+def adagrad(f, x0, bounds, stepsize=1.0, args=[]):
+    fudge_factor = 1e-6  # for numerical stability
+    historical_grad = 0
+    x = x0
+    y = None
+    b = np.array(bounds).T
+
+    def project(z):
+        return np.maximum(b[0], np.minimum(b[1], z))
+
+    while True:
+        y, g = f(x, *args)
+        historical_grad += g.dot(g)
+        adjusted_grad = g / (fudge_factor + np.sqrt(historical_grad))
+        new_x = project(x - stepsize * adjusted_grad)
+        if np.max(np.abs(new_x - x)) < .001:
+            break
+        x = new_x
+    return AdagradResult(x=x, f=y)
+
+
+# Construct time intervals stuff
 def extract_pieces(piece_str):
     '''Convert PSMC-style piece string to model representation.'''
     pieces = []
@@ -26,6 +62,7 @@ def extract_pieces(piece_str):
         pieces += [span] * num
     return pieces
 
+
 def construct_time_points(t1, tK, pieces):
     s = np.logspace(np.log10(t1[-1]), np.log10(tK), sum(pieces) + 1)
     s = s[1:] - s[:-1]
@@ -35,6 +72,7 @@ def construct_time_points(t1, tK, pieces):
         time_points[i] = s[count:(count+p)].sum()
         count += p
     return np.concatenate([t1, time_points])
+
 
 def compress_repeated_obs(dataset):
     # pad with illegal value at starting position
@@ -47,19 +85,19 @@ def compress_repeated_obs(dataset):
     newob[:-1, 0] = csw[1:] - csw[:-1]
     return newob[:-1]
 
+
 def _thin_helper(args):
     thinned = np.array(_smcpp.thin_data(*args), dtype=np.int32)
     return compress_repeated_obs(thinned)
 
+
 def thin_dataset(dataset, thinning):
     '''Only emit full SFS every <thinning> sites'''
-    p = mp.Pool()
-    ret = list(p.map(_thin_helper, [(chrom, thinning, i) for i, chrom in enumerate(dataset)]))
-    p.close()
-    p.join()
-    p.terminate()
-    return ret
-    
+    with mp_pool() as p:
+        return list(p.map(_thin_helper, [(chrom, thinning, i)
+                                         for i, chrom in enumerate(dataset)]))
+
+
 def break_long_spans(dataset, rho, length_cutoff):
     # Spans longer than this are broken up
     # FIXME: should depend on rho
@@ -82,12 +120,14 @@ def break_long_spans(dataset, rho, length_cutoff):
             if s > length_cutoff:
                 obs_list.append(np.insert(obs[cob:x], 0, miss, 0))
                 sums = obs_list[-1].sum(axis=0)
-                s2 = obs_list[-1][:,1][obs_list[-1][:,1]>=0].sum()
+                s2 = obs_list[-1][:, 1][obs_list[-1][:, 1] >= 0].sum()
                 obs_attributes.setdefault(fn, []).append(
                     (positions[cob], positions[x],
                      sums[0], 1. * s2 / sums[0], 1. * sums[2] / sums[0]))
             else:
-                logger.info("omitting sequence length < %d as less than length cutoff %d" % (s, length_cutoff))
+                logger.info("omitting sequence length < %d "
+                            "as less than length cutoff %d" %
+                            (s, length_cutoff))
             cob = x + 1
         s = obs[cob:, 0].sum()
         miss = np.zeros_like(obs[0])
@@ -95,18 +135,24 @@ def break_long_spans(dataset, rho, length_cutoff):
         if s > length_cutoff:
             obs_list.append(np.insert(obs[cob:], 0, miss, 0))
             sums = obs_list[-1].sum(axis=0)
-            s2 = obs_list[-1][:,1][obs_list[-1][:,1]>=0].sum()
-            obs_attributes.setdefault(fn, []).append((positions[cob], positions[-1], sums[0], 1. * s2 / sums[0], 1. * sums[2] / sums[0]))
+            s2 = obs_list[-1][:, 1][obs_list[-1][:, 1] >= 0].sum()
+            obs_attributes.setdefault(fn, []).append((positions[cob],
+                                                      positions[-1], sums[0],
+                                                      1. * s2 / sums[0],
+                                                      1. * sums[2] / sums[0]))
         else:
-            logger.info("omitting sequence length < %d as less than length cutoff %d" % (s, length_cutoff))
+            logger.info("omitting sequence length < %d "
+                        "as less than length cutoff %d" % (s, length_cutoff))
     return obs_list, obs_attributes
+
 
 def balance_hidden_states(model, M):
     M -= 1
     eta = _smcpp.PyRateFunction(model, [])
     ret = [0.0]
-    t = 0
-    for m in range(1, M):
+    # ms = np.arange(0.1, 2.1, .1).tolist() + list(range(3, M))
+    ms = range(1, M)
+    for m in ms:
         def f(t):
             Rt = float(eta.R(t))
             return np.exp(-Rt) - 1.0 * (M - m) / M
@@ -117,8 +163,8 @@ def balance_hidden_states(model, M):
 
 
 def empirical_sfs(data, n):
-    p = mp.Pool()
-    return np.sum(list(p.map(_esfs_helper, ((d, n) for d in data))), axis=0)
+    with mp_pool() as p:
+        return np.sum(list(p.map(_esfs_helper, ((d, n) for d in data))), axis=0)
 
 
 def _esfs_helper(tup):
