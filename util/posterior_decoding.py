6198a8b4d29c2f66f829b23b1986c78dc027c310
Jonathan Terhorst <terhorst@gmail.com>
2016-08-29 09:00:03 -0700

    pd updates

--- util/posterior_decoding.py
+++ util/posterior_decoding.py
@@ -184,7 +184,9 @@ for n in args.n:
     obs = smcpp.util.hmm_data_format(dsub, n, (0, 1)).astype('int32')
     # oo[n] = np.array([c1[1:] for c1 in obs for _ in range(c1[0])])
     # obs[obs[:, 1] != -1, 2:] = 0
-    oo[n] = np.array(smcpp.estimation_tools.thin_dataset([obs], [2]), dtype=np.int32)
+    oo[n] = np.array(smcpp.estimation_tools.thin_dataset([obs], [1]), dtype=np.int32)
+    o = oo[n][0]
+    o[o[:, 1] >= 0, 2:] = 0
     im = smcpp._smcpp.PyOnePopInferenceManager(n - 2, oo[n], hidden_states)
     im.theta = 2.0 * args.N0 * args.theta
     im.rho = 2.0 * args.estimated_rho * args.N0
