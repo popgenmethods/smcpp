4781bd38d93509ebcb73123178a4042ea79640e6
Jonathan Terhorst <terhorst@gmail.com>
2016-11-27 08:26:13 -0800

    simplify plotting code

--- smcpp/commands/plot.py
+++ smcpp/commands/plot.py
@@ -10,7 +10,7 @@ from .. import util, plotting, model
 
 
 def init_parser(parser):
-    parser.add_argument("-g", nargs="+", type=float,
+    parser.add_argument("-g", type=float,
                         help="Plot x-axis in years assuming generation time(s) of g")
     parser.add_argument("-s", "--step-function", action="store_true",
                         help="Plot the step function used to represent the spline "
@@ -22,31 +22,27 @@ def init_parser(parser):
     parser.add_argument("-t", "--offsets", type=float, nargs="+",
                         help="list of offsets, one for each <model>, to shift x axes. "
                              "Mainly useful for plotting aDNA")
-    parser.add_argument("--median", action="store_true",
-                        help="plot median and iqr")
+    # parser.add_argument("--median", action="store_true",
+    #                     help="plot median and iqr")
     parser.add_argument("-x", "--xlim", type=float, nargs=2,
                         default=None, help="x-axis limits")
     parser.add_argument("-y", "--ylim", type=float, nargs=2,
                         default=None, help="y-axis limits")
-    parser.add_argument("-l", "--labels", type=str,
-                        help="label for each plotted function", nargs="+")
+    # parser.add_argument("-l", "--labels", type=str,
+    #                     help="label for each plotted function", nargs="+")
     parser.add_argument("pdf", type=str, help="output PDF", metavar="plot.pdf")
     parser.add_argument("model", type=str, help="SMC++ models to plot", nargs="+")
 
 def main(args):
     psfs = []
-    if args.g is not None and len(args.g) == 1:
-        args.g *= len(args.model)
-    if args.offsets is not None and len(args.offsets) == 1:
-        args.offsets *= len(args.model)
-    if args.labels is None:
-        args.labels = []
     if args.offsets is None:
         args.offsets = []
-    for fn, label, off, g in zip_longest(args.model, args.labels, args.offsets, args.g or [], fillvalue=None):
-        if label == "None": 
-            label = None
+    else:
+        if len(args.offsets) != len(args.model):
+            raise RuntimeError("Please specify one offset per model")
+    for fn, off in zip_longest(args.model, args.offsets, fillvalue=None):
         if fn in ["human", "sawtooth"]:
+            label = None
             p = getattr(util, fn)
             d = {k: p[k] for k in "abs"}
             d['N0'] = p['N0']
@@ -63,13 +59,14 @@ def main(args):
                 d = {'a': m.stepwise_values(), 's': m.s, 'N0': res['N0']}
             else:
                 d = res
-        d['g'] = g
+            label = res['model']['pid']
+        d['g'] = args.g
         psfs.append((label, d, off or 0))
-    if args.median:
-        dmed = {'s': psfs[-1][1]['s'], 'N0': psfs[-1][1]['N0']}
-        for x in "ab":
-            dmed[x] = np.median([p[1][x] for p in psfs], axis=0)
-        psfs.append((None, dmed, 0))
+    # if args.median:
+    #     dmed = {'s': psfs[-1][1]['s'], 'N0': psfs[-1][1]['N0']}
+    #     for x in "ab":
+    #         dmed[x] = np.median([p[1][x] for p in psfs], axis=0)
+    #     psfs.append((None, dmed, 0))
     fig, series = plotting.plot_psfs(psfs, xlim=args.xlim, ylim=args.ylim,
                                      xlabel="Generations" if args.g is None else "Years",
                                      logy=args.logy)
