398c138eda1664497f64213c1b8e6e2de587f627
Jonathan Terhorst <terhorst@gmail.com>
2017-01-04 14:34:39 -0800

    added a form of integration testing

--- .travis/travis.sh
+++ .travis/travis.sh
@@ -5,7 +5,7 @@ BINARY=dist/smcpp-$(git describe --tags)-$TRAVIS_OS_NAME
 if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
     OS=MacOSX
 else
-	OS=Linux
+    OS=Linux
 fi
 
 wget http://repo.continuum.io/miniconda/Miniconda3-latest-$OS-x86_64.sh -O miniconda.sh
@@ -20,6 +20,5 @@ source activate test-environment
 CC=gcc CXX=g++ python setup.py develop
 pip install git+https://github.com/pyinstaller/pyinstaller@483c819
 pyinstaller --clean -s -F --exclude-module PyQt5 --exclude-module PyQt4 --exclude-module mpl_toolkits.tests --exclude-module pyside --exclude-module matplotlib.tests scripts/smc++
-dist/smc++ vcf2smc example/example.vcf.gz example/example.smc.gz 1 msp:msp_0,msp_1,msp_2
-dist/smc++ estimate --theta .00025 --em-iterations 1 example/example.smc.gz
+test/integration/test.sh dist/smc++
 mv dist/smc++ $BINARY
