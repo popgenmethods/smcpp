bf31493b5740de667670f46af97b076d70b9b36c
Jonathan Terhorst <terhorst@gmail.com>
2017-01-14 13:19:59 -0800

    oops

--- conda/template.py
+++ conda/template.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python3
 import sys
 tpl, ver = sys.argv[1:]
-print(open(tpl, "rt").read().format(version=ver[1:]).replace("-", "_"))
+print(open(tpl, "rt").read().format(version=ver[1:].replace("-", "_")))
