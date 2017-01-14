2987550040ea2ed06cd60ff342924ed75b4d119a
Jonathan Terhorst <terhorst@gmail.com>
2017-01-14 13:47:57 -0800

    oops

--- conda/deploy.sh
+++ conda/deploy.sh
@@ -1,7 +1,7 @@
-#!/bin/bash
+#!/bin/bash -x
+set -e
 export PATH="$HOME/miniconda/bin:$PATH"
-conda convert --platform all $HOME/miniconda/conda-bld/**/smcpp-*.tar.bz2 --output-dir conda-bld/
-anaconda -t $ANACONDA_TOKEN upload --force conda-bld/**/smcpp-*.tar.bz2
+anaconda -t $ANACONDA_TOKEN upload --force $HOME/miniconda/conda-bld/*/smcpp-*.tar.bz2
 
 # Next, build 
 constructor conda
