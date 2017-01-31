c43d746a223368bbbe0815adc868537d98eeac3d
Jonathan Terhorst <terhorst@gmail.com>
2017-01-31 09:41:30 -0800

    update citation info

--- smcpp/commands/cite.py
+++ smcpp/commands/cite.py
@@ -1,17 +1,18 @@
 from . import command
 
-BIBTEX = """@article{terhorst2016,
+BIBTEX = """@article{smc++,
     Author = {Terhorst, Jonathan and Kamm, John A and Song, Yun S},
     Journal = {Nature Genetics},
-    Title = {Robust and scalable inference of population history from
-             hundreds of unphased whole genomes},
-    Year = {in press}
+    DOI = {10.1038/ng.3748},
+    Number = {2},
+    Pages = {303--309},
+    Title = {Robust and scalable inference of population history from hundreds of unphased whole genomes},
+    Volume = {49},
+    Year = {2017}
 }
 """
 
-PLAIN = """J. Terhorst, J. A. Kamm, and Y. S. Song. Robust and scalable inference
-of population history from hundreds of unphased whole genomes. Nature
-Genetics, in press.
+PLAIN = """J. Terhorst, J. A. Kamm, and Y. S. Song. Robust and scalable inference of population history from hundreds of unphased whole genomes. Nature Genetics, 49(2):303–309, 2017.
 """
 
 
