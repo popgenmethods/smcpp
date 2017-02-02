ccb0b13b088f8d69552aecbe6a8caefd4f91dc18
Jonathan Terhorst <terhorst@gmail.com>
2017-02-02 15:07:30 -0800

    update citation information

--- smcpp/commands/cite.py
+++ smcpp/commands/cite.py
@@ -6,21 +6,27 @@ BIBTEX = """@article{smc++,
     DOI = {10.1038/ng.3748},
     Number = {2},
     Pages = {303--309},
-    Title = {Robust and scalable inference of population history from hundreds of unphased whole genomes},
+    Title = {Robust and scalable inference of population history 
+             from hundreds of unphased whole genomes},
     Volume = {49},
     Year = {2017}
 }
 """
 
-PLAIN = """J. Terhorst, J. A. Kamm, and Y. S. Song. Robust and scalable inference of population history from hundreds of unphased whole genomes. Nature Genetics, 49(2):303–309, 2017.
+PLAIN = """
+J. Terhorst, J. A. Kamm, and Y. S. Song. Robust and scalable inference
+of population history from hundreds of unphased whole genomes. Nature
+Genetics, 49(2):303–309, 2017.
 """
 
 
 class Cite(command.Command):
     'Print citation information for SMC++'
+    def __init__(self, parser):
+        # don't bother with logging setup for this simple command
+        pass
 
     def main(self, args):
-        print("")
         print(PLAIN)
         print("")
         print(BIBTEX)
