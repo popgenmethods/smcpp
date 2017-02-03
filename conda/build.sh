2a2b48c4e254fa9051befb2c5e549300ae33a2ad
Jonathan Terhorst <terhorst@gmail.com>
2017-02-02 18:59:35 -0800

    ccache debugging

--- conda/build.sh
+++ conda/build.sh
@@ -1,9 +1,10 @@
 #!/bin/bash -ex
 mkdir -p $PREFIX/.bin
 for c in gcc g++; do ln -s $(which ccache) $PREFIX/.bin/$c; done
-export PATH=$PREFIX/.bin:$PATH CCACHE_BASEDIR=$PREFIX
+export PATH=$PREFIX/.bin:$PATH CCACHE_BASEDIR=$PREFIX CCACHE_LOGFILE=/tmp/cache.debug
 echo $(which gcc)
 ccache -s
 python setup.py install --single-version-externally-managed --record=/dev/null
 ccache -s
 rm -rf $PREFIX/.bin
+cat /tmp/cache.debug
