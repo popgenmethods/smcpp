b2a22658ef58e1990f40b48b717e43243aa0730f
Jonathan Terhorst <terhorst@gmail.com>
2017-02-02 17:37:05 -0800

    more informative ccache

--- conda/build.sh
+++ conda/build.sh
@@ -3,4 +3,6 @@ mkdir -p $PREFIX/.bin
 for c in gcc g++; do ln -s $(which ccache) $PREFIX/.bin/$c; done
 export PATH=$PREFIX/.bin:$PATH
 echo $(which gcc)
+ccache -s
 python setup.py install --single-version-externally-managed --record=/dev/null
+ccache -s
