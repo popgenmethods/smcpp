63a5be2867eaca0f180f122bb4ce44965d847a00
Jonathan Terhorst <terhorst@gmail.com>
2017-02-02 18:23:10 -0800

    add CCACHE_BASEDIR for faster compilation

--- conda/build.sh
+++ conda/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -ex
 mkdir -p $PREFIX/.bin
 for c in gcc g++; do ln -s $(which ccache) $PREFIX/.bin/$c; done
-export PATH=$PREFIX/.bin:$PATH
+export PATH=$PREFIX/.bin:$PATH CCACHE_BASEDIR=$PREFIX
 echo $(which gcc)
 ccache -s
 python setup.py install --single-version-externally-managed --record=/dev/null
