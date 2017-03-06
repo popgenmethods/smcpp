b3fa6f2bfa8b03ca84144b42c04cb89cfc2949b0
Jonathan Terhorst <terhorst@gmail.com>
2017-03-05 21:01:08 -0800

    [posterior] allow to decode multiple files

--- smcpp/commands/posterior.py
+++ smcpp/commands/posterior.py
@@ -15,7 +15,8 @@ import json
 
 from .. import _smcpp, util, model, estimation_tools
 from . import command
-
+from smcpp.logging import getLogger
+logger = getLogger(__name__)
 
 class Posterior(command.Command, command.ConsoleCommand):
     "Store/visualize posterior decoding of TMRCA"
@@ -35,10 +36,10 @@ class Posterior(command.Command, command.ConsoleCommand):
         parser.add_argument("--colorbar", action="store_true", help="If plotting, add a colorbar")
         parser.add_argument("model", type=str, metavar="model.final.json",
                             help="SMC++ model to use in forward-backward algorithm")
-        parser.add_argument("data", type=str, metavar="data.smc[.gz]", 
-                            help="SMC++ data set to decode")
         parser.add_argument("output", metavar="arrays.npz", 
                             help="location to save posterior decoding arrays")
+        parser.add_argument("data", type=str, nargs="+", 
+                metavar="data.smc[.gz]", help="SMC++ data set(s) to decode")
 
     def main(self, args):
         command.Command.main(self, args)
@@ -48,48 +49,55 @@ class Posterior(command.Command, command.ConsoleCommand):
         j = json.load(open(args.model, "rt"))
         klass = getattr(model, j['model']['class'])
         m = klass.from_dict(j['model'])
-        contig = estimation_tools.load_data([args.data])[0]
+        contigs = estimation_tools.load_data(args.data)
+        if len(set(c.key for c in contigs)) > 1:
+            logger.error("All data sets must be from same population and have same sample size")
+            sys.exit(1)
         hidden_states = estimation_tools.balance_hidden_states(
-            m.distinguished_model, args.M)
-        obs = contig.data
-        n = np.max(obs[:, 2::2], axis=0)
-        npop = obs.shape[1] // 2 - 1
-        assert len(n) == npop
-        lb = 0 if args.start is None else args.start
-        ub = obs[:, 0].sum() if args.end is None else args.end
-        ## FIXME? Due to the compressed input format the endpoints are only
-        ## approximately picked out.
-        pos = np.cumsum(obs[:, 0])
-        obs = obs[(pos >= lb) & (pos <= ub)]
-        obs = np.insert(obs, 0, [[1, -1] + [0, 0] * npop], 0)
-        L = obs.sum(axis=0)[0]
+            m.distinguished_model, args.M + 1) / (2. * m.distinguished_model.N0)
+        all_obs = []
+        for contig in contigs:
+            obs = contig.data
+            n = np.max(obs[:, 2::2], axis=0)
+            npop = obs.shape[1] // 2 - 1
+            assert len(n) == npop
 
+            lb = 0 if args.start is None else args.start
+            ub = obs[:, 0].sum() if args.end is None else args.end
+            ## FIXME? Due to the compressed input format the endpoints are only
+            ## approximately picked out.
+            pos = np.cumsum(obs[:, 0])
+            obs = obs[(pos >= lb) & (pos <= ub)]
+            obs = np.insert(obs, 0, [[1, -1] + [0, 0] * npop], 0)
+            all_obs.append(obs)
         # Perform thinning, if requested
         if args.thinning > 1:
-            obs = estimation_tools.thin_dataset([obs], [args.thinning])[0]
+            all_obs = estimation_tools.thin_dataset(all_obs, [args.thinning] * len(all_obs))
         if npop == 1:
             im = _smcpp.PyOnePopInferenceManager(
-                n[0], [obs], hidden_states, contig.key, args.polarization_error)
+                n[0], all_obs, hidden_states, contig.key, args.polarization_error)
         else:
             assert npop == 2
             im = _smcpp.PyTwoPopInferenceManager(
-                n[0], n[1], [obs], hidden_states, contig.key, args.polarization_error)
+                n[0], n[1], all_obs, hidden_states, contig.key, args.polarization_error)
         im.theta = j['theta']
         im.save_gamma = True
         im.model = m
         im.E_step()
-        gamma = im.gammas[0]
-        gamma /= gamma.sum(axis=0)
-        # gamma = np.zeros([args.M, L])
-        # sp = 0
-        # for row, col in zip(obs, im.gammas[0].T):
-        #     an = row[0]
-        #     gamma[:, sp:(sp + an)] = col[:, None] / an
-        #     sp += an
-        np.savez_compressed(args.output, hidden_states=hidden_states, 
-                            sites=obs[:, 0], posterior=gamma)
+        gammas = im.gammas
+        for g in gammas:
+            Lr = g.sum(axis=0)
+            g /= Lr
+            L = Lr.sum()
+        kwargs = {path: g for path, g in zip(args.data, gammas)}
+        kwargs.update({path + "_sites": obs[:, 0] for path, obs in zip(args.data, all_obs)})
+        np.savez_compressed(args.output, hidden_states=hidden_states, **kwargs)
         if args.heatmap:
+            if len(args.data) > 1:
+                logger.error("--heatmap is only supported for one data set")
+                sys.exit(1)
             # Plotting code
+            gamma = g
             fig, ax = plt.subplots()
             x = np.insert(np.cumsum(obs[:, 0]), 0, 0)
             y = hidden_states[:-1]
