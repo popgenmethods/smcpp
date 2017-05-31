b3efa20af73ecfb40eff698e3b1585ffe4a3eb2f
Jonathan Terhorst <terhorst@gmail.com>
2017-05-31 12:12:34 -0700

    [test] improve test coverage

--- test/integration/test.sh
+++ test/integration/test.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -x
 SMC=$1
 set -e
-$SMC vcf2smc -v example/example.vcf.gz /tmp/example.1.smc.gz 1 msp1:msp_0,msp_1
-$SMC vcf2smc -d msp_2 msp_2 example/example.vcf.gz /tmp/example.2.smc.gz 1 msp2:msp_2
-$SMC vcf2smc -d msp_1 msp_1 example/example.vcf.gz /tmp/example.12.smc.gz 1 msp1:msp_0,msp_1 msp2:msp_2
+$SMC vcf2smc -v example/example.vcf.gz /tmp/example.1.smc.gz 1 msp1:msp_0,msp_1,msp_2
+$SMC vcf2smc -d msp_0 msp_0 example/example.vcf.gz /tmp/example.2.smc.gz 1 msp2:msp_0,msp_3,msp_4
+$SMC vcf2smc -d msp_1 msp_1 example/example.vcf.gz /tmp/example.12.smc.gz 1 msp1:msp_1,msp_2 msp2:msp_3,msp_4,msp_0
 $SMC estimate -o /tmp/out/1 --unfold --em-iterations 1 1.25e-8 /tmp/example.1.smc.gz
 $SMC estimate -p 0.01 -o /tmp/out/2 --em-iterations 1 1.25e-8 /tmp/example.2.smc.gz
 $SMC split -o /tmp/out/split --em-iterations 1 \
