ef3781ae42365a6ce1130414ef9af5ded6b23361
Jonathan Terhorst <terhorst@gmail.com>
2017-09-22 12:41:34 -0400

    [build] streamline the process and fix incompatibilies with conda-build 3.x

--- conda/deploy.sh
+++ conda/deploy.sh
@@ -1,7 +1,9 @@
 #!/bin/bash -x
 set -e
 export PATH="$HOME/miniconda/bin:$PATH"
-anaconda -t $ANACONDA_TOKEN upload --force $(conda build --output conda)
+PKGS=$(conda build -c terhorst -c conda-forge -c bioconda conda --output)
+PKGS=$(cat .packages)
+anaconda -t $ANACONDA_TOKEN upload --force $PKGS
 
-# Next, build 
+# Next, pull uploaded pkg and create a binary installed
 constructor conda
