9a404bd49fbefbc208c49e6eab667a9ca5b2fccb
Jonathan Terhorst <terhorst@gmail.com>
2017-10-31 14:23:22 -0400

    [core] minor

--- setup.py
+++ setup.py
@@ -12,11 +12,11 @@ from Cython.Build import cythonize
 import numpy as np
 
 if True:
-    extra_compile_args = ["-O2", "-std=c++11", "-Wno-deprecated-declarations", "-DNO_CHECK_NAN", 
+    extra_compile_args = ["-O2", "-std=c++11", "-Wno-deprecated-declarations", '-DNO_CHECK_NAN',
                           '-fopenmp', '-g', '-DEIGEN_DONT_PARALLELIZE']
 else:
-    extra_compile_args = ["-O0", "-g", "-std=c++11", "-Wno-deprecated-declarations",
-                          "-D_GLIBCXX_DEBUG", "-Wfatal-errors"]
+    extra_compile_args = ["-O0", "-g", "-std=c++11", "-Wno-deprecated-declarations", "-fopenmp",
+                          "-D_GLIBCXX_DEBUG", "-Wfatal-errors", "-DEIGEN_DONT_PARALLELIZE"]
 
 extra_link_args=['-fopenmp']
 libraries = ['mpfr', 'gmp', 'gmpxx', 'gsl', 'gslcblas']
