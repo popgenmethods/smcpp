50577be286e35d7f4000141edaca4be7a98771da
Jonathan Terhorst <terhorst@gmail.com>
2018-01-30 10:52:01 -0500

    build updates

--- conda/build.sh
+++ conda/build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash -ex
 export CFLAGS="-I$PREFIX/include $CFLAGS"
+export CXXFLAGS="-Wno-int-in-bool-context $CXXFLAGS"
 python setup.py install --single-version-externally-managed --record=/dev/null
