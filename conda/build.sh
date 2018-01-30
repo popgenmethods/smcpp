6c0a76439172252cfe0ab7f22b3b50c6ae4bf4fe
Jonathan Terhorst <terhorst@gmail.com>
2018-01-30 10:26:53 -0500

    build updates

--- conda/build.sh
+++ conda/build.sh
@@ -1,8 +1,3 @@
 #!/bin/bash -ex
-CFLAGS="-I$PREFIX/include -Wno-int-in-bool-context -mtune=generic $CFLAGS"
-CXXFLAGS="-Wno-int-in-bool-context -I$PREFIX/include -mtune=generic $CXXFLAGS"
-LDFLAGS="-L$PREFIX/lib $LDFLAGS"
-PATH="$PREFIX/bin:$PATH"
-LDSHARED=$(python -c "import sys;from distutils import sysconfig;lds = sysconfig.get_config_var('LDSHARED').split(' ');print(' '.join(['$GCC'] + lds[1:]))")
-export CFLAGS CXXFLAGS LDFLAGS PATH LDSHARED
-$PYTHON setup.py install --single-version-externally-managed --record=/dev/null
+export CFLAGS="-I$PREFIX/include $CFLAGS"
+python setup.py install --single-version-externally-managed --record=/dev/null
