8d7448699474567c4638532d6943cba524054b22
Jonathan Terhorst <terhorst@gmail.com>
2018-03-10 15:39:53 -0500

    [estimate] refactor t1,tK options. fixes #75 and #74

--- smcpp/analysis.py
+++ smcpp/analysis.py
@@ -48,7 +48,7 @@ class BaseAnalysis:
         pipe.add_filter(data_filter.BreakLongSpans(cutoff=100000))
         pipe.add_filter(data_filter.DropSmallContigs(100000))
         pipe.add_filter(watterson=data_filter.Watterson())
-        pipe.add_filter(mutation_counts=data_filter.CountMutations(w=int(1e-2 / self._rho)))
+        pipe.add_filter(mutation_counts=data_filter.CountMutations(w=int(2e-3 * self._N0 / self._rho)))
 
 
     @property
@@ -198,29 +198,35 @@ class Analysis(BaseAnalysis):
             self._theta = d['theta']
             self._rho = d['rho']
             self._model = _model_cls_d[d['model']['class']].from_dict(d['model'])
-            hs = self.rescale(smcpp.estimation_tools.balance_hidden_states(self._model, args.hs * args.knots))
+            hs = self.rescale(smcpp.estimation_tools.balance_hidden_states(self._model, args.knots))
             self._hidden_states = {k: hs for k in self.populations}
-            self._knots = hs[1:-1:args.hs]
+            self._knots = hs[1:-1]
             logger.debug("rebalanced hidden states: %s", self._hidden_states)
         else:
-            mc = self._pipeline['mutation_counts'].counts 
-            w = self._pipeline['mutation_counts'].w
-            q = smcpp.beta_de.quantile(mc / w, 1. / w, np.geomspace(1e-2, .99, args.hs * args.knots))
-            tau = q / (2. * self._theta)
-            # Window length is w^2 (1e4 by default)
-            # # mutations ~ Pois(theta * 2 * w^2 * lambda)
-            # tau ~= mc / 
-            self._knots = tau[::args.hs]
-            if args.t1 is None:
+            if args.timepoints == "h":
+                mc = self._pipeline['mutation_counts'].counts
+                w = self._pipeline['mutation_counts'].w
+                q = smcpp.beta_de.quantile(mc / w, 1. / w, np.geomspace(1e-2, .99, args.knots))
+                tau = q / (2. * self._theta)
+                self._knots = tau
                 n = np.average([c.n[0] for c in self.contigs], 
                         weights=[len(c) for c in self.contigs])
-                n += 2  # distinguished lineages
-                args.t1 = 2. * NeN0 * self._N0 * -np.log(.001) / (n * (n - 1) / 2)
-                logger.info("calculated t1: %f gens", args.t1)
-            rt1 = self.rescale(args.t1)
-            if rt1 < self._knots[0]:
-                self._knots = np.r_[np.geomspace(rt1, self._knots[0], args.knots // 4, False), self._knots]
-            hs = np.r_[0., tau, np.inf]
+                if n > 0:
+                    t1 = 2. * NeN0 * self._N0 * -np.log(.001) / (n * (n - 1) / 2)
+                    logger.info("calculated t1: %f gens", t1)
+                    rt1 = self.rescale(t1)
+                    if rt1 < self._knots[0]:
+                        self._knots = np.r_[np.geomspace(rt1, self._knots[0], args.knots // 4, False), self._knots]
+                logger.debug("Determined knots heuristically to be: %s", self._knots)
+            else:
+                try:
+                    t1, tK = [float(x) / 2. / self._N0 for x in args.timepoints.split(",")]
+                    self._knots = np.geomspace(t1, tK, args.knots, False)
+                    logger.debug("Knots are: %s", self._knots)
+                except:
+                    raise RuntimeError("Could not parse time points. "
+                                       "See documentation for --timepoints option.")
+            hs = np.r_[0., self._knots, np.inf]
             self._hidden_states = {k: hs for k in self.populations}
 
         logger.debug("hidden states in coalescent scaling: %s", hs)
