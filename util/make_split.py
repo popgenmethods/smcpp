a059152918de8bcb0f19259a6dfa748ba083f8f7
Jonathan Terhorst <terhorst@gmail.com>
2018-04-12 21:35:42 -0400

    [test] split testing

--- util/make_split.py
+++ util/make_split.py
@@ -2,16 +2,21 @@
 import msprime as msp
 import sys
 
-de = [msp.MassMigration(time=5000,
-                            source=1, 
-                            destination=0, 
-                            proportion=1.0)]
-pc = [msp.PopulationConfiguration(sample_size = 20) for _ in range(2)]
+de = [
+    msp.PopulationParametersChange(time=0, population_id=1,
+                                   initial_size=500000),
+    msp.PopulationParametersChange(time=5000, population_id=1,
+                                   initial_size=20000),
+    msp.MassMigration(time=15000,
+                      source=1,
+                      destination=0,
+                      proportion=1.0)]
+pc = [msp.PopulationConfiguration(sample_size = 50) for _ in range(2)]
 
 tree_seq = msp.simulate(population_configurations=pc, 
                         demographic_events=de,
                         recombination_rate=1e-9,
                         mutation_rate=1e-8,
                         length=int(sys.argv[1]),
-                        Ne=50000)
+                        Ne=10000)
 tree_seq.write_vcf(sys.stdout, ploidy=2)
--- util/run_split_test.sh
+++ util/run_split_test.sh
@@ -2,15 +2,15 @@
 SMC=$(which smc++)
 set -e
 LENGTH=100000000
-./make_split.py $LENGTH > /tmp/split.vcf
-bcftools view -Oz -o /tmp/split.vcf.gz /tmp/split.vcf
-bcftools index /tmp/split.vcf.gz
-$SMC vcf2smc --length $LENGTH -v /tmp/split.vcf.gz /tmp/example.1.smc.gz 1 msp1:$(echo msp_{0..9} | sed 's/ /,/g')
-$SMC vcf2smc --length $LENGTH -v /tmp/split.vcf.gz /tmp/example.2.smc.gz 1 msp2:$(echo msp_{10..19} | sed 's/ /,/g')
-$SMC vcf2smc --length $LENGTH -d msp_0 msp_0 /tmp/split.vcf.gz /tmp/example.12.smc.gz 1 msp1:$(echo msp_{0..9} | sed 's/ /,/g') msp2:$(echo msp_{10..19} | sed 's/ /,/g')
-$SMC vcf2smc --length $LENGTH -d msp_10 msp_10 /tmp/split.vcf.gz /tmp/example.21.smc.gz 1 msp2:$(echo msp_{10..19} | sed 's/ /,/g') msp1:$(echo msp_{0..9} | sed 's/ /,/g')
-$SMC estimate -o /tmp/out/1 -v 1e-8 /tmp/example.1.smc.gz --spline piecewise
-$SMC estimate -o /tmp/out/2 -v 1e-8 /tmp/example.2.smc.gz --spline piecewise
+# ./make_split.py $LENGTH > /tmp/split.vcf
+# bcftools view -Oz -o /tmp/split.vcf.gz /tmp/split.vcf
+# bcftools index /tmp/split.vcf.gz
+# $SMC vcf2smc --length $LENGTH -v /tmp/split.vcf.gz /tmp/example.1.smc.gz 1 msp1:$(echo msp_{0..24} | sed 's/ /,/g')
+# $SMC vcf2smc --length $LENGTH -v /tmp/split.vcf.gz /tmp/example.2.smc.gz 1 msp2:$(echo msp_{25..49} | sed 's/ /,/g')
+# $SMC vcf2smc --length $LENGTH -d msp_0 msp_0 /tmp/split.vcf.gz /tmp/example.12.smc.gz 1 msp1:$(echo msp_{0..24} | sed 's/ /,/g') msp2:$(echo msp_{25..49} | sed 's/ /,/g')
+# $SMC vcf2smc --length $LENGTH -d msp_10 msp_10 /tmp/split.vcf.gz /tmp/example.21.smc.gz 1 msp2:$(echo msp_{25..49} | sed 's/ /,/g') msp1:$(echo msp_{0..24} | sed 's/ /,/g')
+$SMC estimate -o /tmp/out/1 -v 1e-8 /tmp/example.1.smc.gz
+$SMC estimate -o /tmp/out/2 -v 1e-8 /tmp/example.2.smc.gz
 $SMC split -o /tmp/out/split --em-iterations 1 -v \
     /tmp/out/1/model.final.json \
     /tmp/out/2/model.final.json \
