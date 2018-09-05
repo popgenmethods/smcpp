e9815e3f609f4247f694e415d80fecdb292eca2e
Jonathan Terhorst <terhorst@gmail.com>
2018-09-05 13:57:19 -0400

    updates

--- smcpp/analysis/analysis.py
+++ smcpp/analysis/analysis.py
@@ -27,11 +27,12 @@ class Analysis(base.BaseAnalysis):
         m[:] = np.log(NeN0)
         hs = estimation_tools.balance_hidden_states(m, 2 + args.knots)
         if args.timepoints is not None:
-            hs = np.geomspace(args.timepoints[0], args.timepoints[1], args.knots)
+            t1, tK = [x / 2 / self._N0 for x in args.timepoints]
+        else:
+            t1 = tK = None
         hs /= (2 * self._N0)
         self.hidden_states = hs
-        self._knots = hs[1:-1]
-        logger.debug("Knots are: %s", self._knots)
+        self._init_knots(hs, t1, tK)
 
         self._init_model(args.spline)
         hs0 = hs
@@ -45,11 +46,19 @@ class Analysis(base.BaseAnalysis):
             args.algorithm,
             args.xtol,
             args.ftol,
-            learn_rho=args.r is None,
+            learn_rho=False,
             single=False
         )
         self._init_regularization(args)
         self.run(1)
+        hs = estimation_tools.balance_hidden_states(m, 2 + args.knots) / 2 / self._N0
+        self.hidden_states = hs
+        self._init_knots(hs, t1, tK)
+        m = self._model
+        # import ipdb
+        # ipdb.set_trace()
+        self._init_model(args.spline)
+        self._model[:] = np.log(m(self._knots))
 
         pipe = self._pipeline
         pipe.add_filter(data_filter.Thin(thinning=args.thinning))
@@ -60,7 +69,6 @@ class Analysis(base.BaseAnalysis):
         pipe.add_filter(data_filter.DropUninformativeContigs())
         pipe.add_filter(data_filter.Summarize())
 
-        self.hidden_states = hs0
         self._init_inference_manager(args.polarization_error, self.hidden_states)
         self.alpha = 1
         self._init_optimizer(
@@ -85,6 +93,20 @@ class Analysis(base.BaseAnalysis):
         }[spline_class]
         assert self.npop == 1
         self._model = SMCModel(self._knots, self._N0, spline_class, self.populations[0])
+
+    def _init_knots(self, hs, t1, tK):
+        self._knots = hs[1:-1:2]
+        mult = np.mean(self._knots[1:] / self._knots[:-1])
+        t = t1
+        k0 = self._knots[0]
+        a = []
+        while t < k0:
+            a = np.r_[a, t]
+            t *= mult
+        self._knots = np.r_[a, self._knots]
+        if tK > self._knots[-1]:
+            self._knots = np.r_[self._knots, tK]
+        logger.debug("Knots are: %s", self._knots)
 
     def _init_regularization(self, args):
         if self._args.lambda_:
